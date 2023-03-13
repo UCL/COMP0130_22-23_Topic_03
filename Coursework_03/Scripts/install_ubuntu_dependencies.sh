3fb5f58086b4ea5197c95f072b43993d764060e7
Oli Kingshott <oliver@kingshott.com>
2023-03-13 15:42:35 +0000

    add unzip to ubuntu deps

--- Coursework_03/Scripts/install_ubuntu_dependencies.sh
+++ Coursework_03/Scripts/install_ubuntu_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install cmake build-essential libeigen3-dev libboost-dev libboost-filesystem-dev libblas-dev liblapack-dev libopencv-dev libglew-dev mesa-utils libgl1-mesa-glx
+sudo apt install cmake build-essential libeigen3-dev libboost-dev libboost-filesystem-dev libblas-dev liblapack-dev libopencv-dev libglew-dev mesa-utils libgl1-mesa-glx unzip
